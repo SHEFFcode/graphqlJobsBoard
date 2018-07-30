94373016354cef855744a2b658353397f0d8ebcc
SHEFFmachine <sheff@sheffmachine.com>
2018-07-29 19:30:36 -0700

    get started on the jobsBoard graphql tut

--- server/server.js
+++ server/server.js
@@ -1,28 +1,43 @@
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const express = require('express');
-const expressJwt = require('express-jwt');
-const jwt = require('jsonwebtoken');
-const db = require('./db');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const expressJwt = require('express-jwt')
+const jwt = require('jsonwebtoken')
+const db = require('./db')
+const { makeExecutableSchema } = require('graphql-tools')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const fs = require('fs')
+const Query = require('./resolvers')
 
-const port = 9000;
-const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+const port = 9000
+const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64')
 
-const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+const typeDefs = fs.readFileSync('./schema.graphql', { encoding: 'utf-8' })
+const resolvers = Query
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const app = express()
+app.use(
+  cors(),
+  bodyParser.json(),
+  expressJwt({
+    secret: jwtSecret,
+    credentialsRequired: false
+  })
+)
+
+app.use('/graphql', graphqlExpress({ schema }))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = db.users.list().find((user) => user.email === email);
+  const { email, password } = req.body
+  const user = db.users.list().find(user => user.email === email)
   if (!(user && user.password === password)) {
-    res.sendStatus(401);
-    return;
+    res.sendStatus(401)
+    return
   }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
-});
+  const token = jwt.sign({ sub: user.id }, jwtSecret)
+  res.send({ token })
+})
 
-app.listen(port, () => console.info(`Server started on port ${port}`));
+app.listen(port, () => console.info(`Server started on port ${port}`))
